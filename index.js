c06746c9aa62147046e6c8d9c13acdf863aac313
Thasi Pablo <39631185+thasipablo@users.noreply.github.com>
2023-06-26 23:51:54 +0100

    hide mobile menu when clicking on exit button

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const menuButton = document.querySelector('.menu');
 const navbar = document.querySelector('.navbar');
 const hamburgerMenuBtn = document.querySelector('.hamburger_menu');
 const mobileMenu = document.querySelector('.mobile_menu');
+const exitBtn = document.querySelector('.exit');
 
-hamburgerMenuBtn.addEventListener('click', function() {
+hamburgerMenuBtn.addEventListener('click', () => {
+  navbar.classList.toggle('hidden');
+  mobileMenu.classList.toggle('hidden');
+});
+
+exitBtn.addEventListener('click', () => {
   navbar.classList.toggle('hidden');
   mobileMenu.classList.toggle('hidden');
 });
