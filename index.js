abd21090b13f45a671bc714621c26fececb29dc3
Thasi Pablo <39631185+thasipablo@users.noreply.github.com>
2023-06-27 22:48:10 +0100

    create popup element

--- index.js
+++ index.js
@@ -102,3 +102,21 @@ projects.forEach((project) => {
   `;
   projectsListTag.appendChild(projectCard);
 });
+
+const projectCardPopup = document.createElement("section");
+projectCardPopup.classList.add("project-card-popup");
+projectCardPopup.classList.add("hidden");
+projectCardPopup.innerHTML = `
+  <h2 class="header-2">${project.name}</h2>
+  <p class="para-2">${project.description}</p>
+  <ul class="apps-1">
+    <li class="i">${project.technologies[0]}</li>
+    <li class="i">${project.technologies[1]}</li>
+    <li class="i">${project.technologies[2]}</li>
+  </ul>
+  <button class="btn-5">
+    See Project
+  </button>
+`;
+
+projectsListTag.appendChild(projectCardPopup);
