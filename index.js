6f361e562558b7df137106b944a5e71b277ca378
codeStackr <rdzaman20000@gmail.com>
2023-06-27 20:51:15 +0600

    Add mobilemenu disappear when link click feature

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const navbar = document.querySelector('.navbar');
 const hamburgerMenuBtn = document.querySelector('.hamburger_menu');
 const mobileMenu = document.querySelector('.mobile_menu');
 const exitBtn = document.querySelector('.exit');
+const navLink =  document.querySelectorAll('.nav_link');
 
 hamburgerMenuBtn.addEventListener('click', () => {
   navbar.classList.toggle('hidden');
@@ -11,4 +12,11 @@ hamburgerMenuBtn.addEventListener('click', () => {
 exitBtn.addEventListener('click', () => {
   navbar.classList.toggle('hidden');
   mobileMenu.classList.toggle('hidden');
+});
+
+navLink.forEach((link) => {
+  link.addEventListener('click', () => {
+    navbar.classList.toggle('hidden');
+    mobileMenu.classList.add('hidden');
+  });
 });
