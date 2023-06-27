7eb0879ca69f8180b4bbf23b4172fe6da2e01925
codeStackr <rdzaman20000@gmail.com>
2023-06-27 20:58:28 +0600

    fix styleint and eslint errors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const navbar = document.querySelector('.navbar');
 const hamburgerMenuBtn = document.querySelector('.hamburger_menu');
 const mobileMenu = document.querySelector('.mobile_menu');
 const exitBtn = document.querySelector('.exit');
-const navLink =  document.querySelectorAll('.nav_link');
+const navLink = document.querySelectorAll('.nav_link');
 
 hamburgerMenuBtn.addEventListener('click', () => {
   navbar.classList.toggle('hidden');
