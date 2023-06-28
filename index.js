1bd6e3effe1e5292774e9f4f116429294ee7ce98
codeStackr <rdzaman20000@gmail.com>
2023-06-29 04:04:58 +0600

    fix the name input type

--- index.js
+++ index.js
@@ -36,6 +36,11 @@ formtag.addEventListener('submit', (event) => {
 });
 
 // #endregion
+
+//#region firstname lastname required change
+if
+//#endregion
+
 // projects cards
 const projects = [
   {
@@ -116,20 +121,20 @@ projects.forEach((project) => {
   projectsListTag.appendChild(projectCard);
 });
 
-const projectCardPopup = document.createElement('section');
-projectCardPopup.classList.add('project-card-popup');
-projectCardPopup.classList.add('hidden');
-projectCardPopup.innerHTML = `
-  <h2 class="header-2">${project.name}</h2>
-  <p class="para-2">${project.description}</p>
-  <ul class="apps-1">
-    <li class="i">${project.technologies[0]}</li>
-    <li class="i">${project.technologies[1]}</li>
-    <li class="i">${project.technologies[2]}</li>
-  </ul>
-  <button class="btn-5">
-    See Project
-  </button>
-`;
+// const projectCardPopup = document.createElement('section');
+// projectCardPopup.classList.add('project-card-popup');
+// projectCardPopup.classList.add('hidden');
+// projectCardPopup.innerHTML = `
+//   <h2 class="header-2">${project.name}</h2>
+//   <p class="para-2">${project.description}</p>
+//   <ul class="apps-1">
+//     <li class="i">${project.technologies[0]}</li>
+//     <li class="i">${project.technologies[1]}</li>
+//     <li class="i">${project.technologies[2]}</li>
+//   </ul>
+//   <button class="btn-5">
+//     See Project
+//   </button>
+// `;
 
 projectsListTag.appendChild(projectCardPopup);
